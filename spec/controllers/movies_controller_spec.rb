c9430cb414381bbeec28e04157d24ab3bb1b5599
Tai LE <lhtai181@gmail.com>
2022-08-04 15:03:56 +0700

    update the rest specs

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -42,31 +42,95 @@ RSpec.describe MoviesController do
   end
 
   describe 'POST #create' do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:title) { 'title' }
+    let(:description) { 'description' }
+    let(:link) { 'link' }
+    let(:params) do
+      {
+        movie: {
+          title: title,
+          description: description,
+          link: link
+        }
+      }
+    end
+
+    before do
+      allow_any_instance_of(MoviesController).to receive(:current_user).and_return(user)
+    end
+
     it 'creates movie successfully' do
-      expect(true).to eq false
+      post :create, params
+
+      expect(response).to have_http_status :ok
+      body = JSON.parse response.body
+      expect(body['title']).to        eq title
+      expect(body['description']).to  eq description
+      expect(body['link']).to         eq link
+      expect(body['shared_by']).to    eq user.email
     end
 
     context 'Not sign in' do
+      let(:message) { 'not_authorized' }
+
+      before do
+        allow_any_instance_of(MoviesController).to receive(:current_user).and_return(nil)
+      end
+
       it 'returns error' do
-        expect(true).to eq false
+        post :create, params
+
+        expect(response).to have_http_status :unprocessable_entity
+        expect(JSON.parse(response.body)['message']).to eq message
       end
     end
 
     context 'title is empty' do
+      let(:title) { nil }
+      let(:errors) do
+        {
+          'title' => ['must present']
+        }
+      end
+
       it 'returns error' do
-        expect(true).to eq false
+        post :create, params
+
+        expect(response).to have_http_status :unprocessable_entity
+        expect(JSON.parse(response.body)['errors']).to include(errors)
       end
     end
 
     context 'description is empty' do
+      let(:description) { nil }
+      let(:errors) do
+        {
+          'description' => ['must present']
+        }
+      end
+
       it 'returns error' do
-        expect(true).to eq false
+        post :create, params
+
+        expect(response).to have_http_status :unprocessable_entity
+        expect(JSON.parse(response.body)['errors']).to include(errors)
       end
     end
 
     context 'link is empty' do
+      let(:link) { nil }
+      let(:errors) do
+        {
+          'link' => ['must present']
+        }
+      end
+
       it 'returns error' do
-        expect(true).to eq false
+        post :create, params
+
+        expect(response).to have_http_status :unprocessable_entity
+        expect(JSON.parse(response.body)['errors']).to include(errors)
       end
     end
   end
