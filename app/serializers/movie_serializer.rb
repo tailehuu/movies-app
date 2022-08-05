8d9136cba5f36f620e4078203d51c9cc2fe8dd27
Tai LE <lhtai181@gmail.com>
2022-08-05 11:37:33 +0700

    - response number_votes_up & number_votes_down - response created_at as timestamp

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,7 +1,11 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :link, :shared_by, :created_at
+  attributes :id, :title, :description, :link, :number_votes_up, :number_votes_down, :shared_by, :created_at
 
   def shared_by
     object.user.email
+  end
+
+  def created_at
+    object.created_at.to_i
   end
 end
